558bdd597130c829e909acfbd055b6cedf4240bf
Rishav Kishore <229695296+Rishav-KE@users.noreply.github.com>
2026-01-04 20:14:21 +0530

    LLD-Google-Docs

---
+++ LLD/LLD/Google-Docs-1/googledocs1.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+// Abstraction for document element
+class DocumentElement{
+    public:
+    virtual string render()=0;
+};
+
+// concrete element for text elements
+class TextElement: public DocumentElement{
+    private:
+    string text;
+
+    public:
+    TextElement(string text){
+        this->text=text;
+    }
+
+    string render() override{
+        return text;
+    }
+};
+
+// concrete implementation for image elements
+class ImageElement : public DocumentElement{
+    private:
+    string imagePath;
+
+    public:
+    ImageElement(string imagePath){
+        this->imagePath=imagePath;
+    }
+
+    string render() override{
+        return "[Image: "+imagePath+ "]";
+    }
+};
+
+// NewLineElement represents a Line break in the document.
+class NewLineElement : public DocumentElement{
+    public:
+    string render() override{
+        return "\n";
+    }
+};
+
+// Tabspace element represents a tab space in the document.
+class TabSpaceElement: public DocumentElement{
+    public:
+    string render() override{
+        return "\t";
+    }
+};
+
+// Document class responsible for holding a collection of elements
+class Document{
+    private:
+    vector<DocumentElement*>documentElements;
+
+    public:
+    void addElement(DocumentElement* element){
+        documentElements.push_back(element);
+    }
+
+    // Renders the document by concatenating the render ouptut of all elements
+
+    string render(){
+        string result;
+        for(auto element:documentElements){
+            result+=element->render();
+        }
+        return result;
+    }
+
+};
+
+// Persistance Interface
+class Persistance{
+    public:
+    virtual void save(string data)=0;
+};
+
+class FileStorage : public Persistance{
+    public:
+    void save(string data) override{
+        ofstream outFile("document.txt");
+        if(outFile){
+            outFile << data;
+            outFile.close();
+            cout << "Document save to document.txt" <<endl;
+        }else{
+            cout<<"Error: Unable to open file for writing."<<endl;
+        }
+    }
+
+};
+
+// Persistance DB storage implementation
+class DBStorage: public Persistance{
+    public:
+    void save(string data) override{
+        // save to db logic may be the sql logic
+    }
+};
+
+// Document Editor class managing client interactions
+class DocumentEditor{
+    private:
+    Document* document;
+    Persistance* storage;
+    string renderedDocument;
+
+    public:
+    DocumentEditor(Document* document, Persistance* storage){
+        this->document=document;
+        this->storage=storage;
+    }
+
+    void addText(string text){
+        document->addElement(new TextElement(text));
+    }
+
+    void addImage(string imagePath){
+        document->addElement(new ImageElement(imagePath));
+    }
+
+    // Adds a new line to the document.
+    void addNewLine(){
+        document->addElement(new NewLineElement());
+    }
+
+    // Adds a tab space to the document
+    void addTabSpace(){
+        document->addElement(new TabSpaceElement());
+    }
+
+    string renderDocument(){
+        if(renderedDocument.empty()){
+            renderedDocument=document->render();
+        }
+        return renderedDocument;
+    }
+
+    void saveDocument(){
+        storage->save(renderDocument());
+    }
+};
+
+//Client usage example
+
+int main(){
+    Document* document=new Document();
+    Persistance* persistance=new FileStorage();
+    DocumentEditor* editor= new DocumentEditor(document, persistance);
+
+    // Simulate a client using the editor with common text formatting features.
+
+    editor->addText("Hello, world!");
+    editor->addNewLine();
+    editor->addText("This is a real-world document editor example");
+    editor->addNewLine();
+    editor->addImage("picture.jpg");
+
+    // Render and display the final document
+    cout<< editor->renderDocument() <<endl;
+    editor->saveDocument();
+    return 0;
+}
